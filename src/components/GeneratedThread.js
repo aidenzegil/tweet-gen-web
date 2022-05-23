a2133c5426f6096e3f3d20d8400669ec6262a17d
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-23 11:19:38 -0400

    fixed component

--- src/components/GeneratedThread.js
+++ src/components/GeneratedThread.js
@@ -1,40 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-// const GeneratedThread = (numberOfThreads, thread) => {
-//   const [shown, setShown] = useState(["", "", "", ""])
-//   const [oldThread, setOldThread] = useState(thread)
-//   if (oldThread !== thread) {
-//     setOldThread(thread)
-//     setShown(thread)
-//   }
-
-//   const displayedInputs = () => {
-//     const inputs = [];
-//     for (var i = 0; i < numberOfThreads; i++) {
-//       inputs.push(
-//         <textarea
-//           className="Input"
-//           maxLength="280"
-//           value = {shown[i]}
-//           onChange={(e) => setShown(shown.splice(i, 1, e.target.value))}
-//         />
-//       );
-//     }
-//     return inputs;
-//   };
-
-//   const displayed = displayedInputs()
-
-//   return (
-//     <div>
-//       {displayed}
-//     </div>
-//   );
-// };
-
-// export default GeneratedThread;
-
 const GeneratedThread = (numThreads, thread, textArea = []) => {
   const [shown, setShown] = useState(["", "", "", ""])
   const [oldThread, setOldThread] = useState(thread)
