5f02ac6c988cec500bc5898026a1080d386de13e
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-23 11:18:57 -0400

    fixed components

--- src/components/GeneratedThread.js
+++ src/components/GeneratedThread.js
@@ -1,37 +1,72 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-const GeneratedThread = (numberOfThreads, thread) => {
+// const GeneratedThread = (numberOfThreads, thread) => {
+//   const [shown, setShown] = useState(["", "", "", ""])
+//   const [oldThread, setOldThread] = useState(thread)
+//   if (oldThread !== thread) {
+//     setOldThread(thread)
+//     setShown(thread)
+//   }
+
+//   const displayedInputs = () => {
+//     const inputs = [];
+//     for (var i = 0; i < numberOfThreads; i++) {
+//       inputs.push(
+//         <textarea
+//           className="Input"
+//           maxLength="280"
+//           value = {shown[i]}
+//           onChange={(e) => setShown(shown.splice(i, 1, e.target.value))}
+//         />
+//       );
+//     }
+//     return inputs;
+//   };
+
+//   const displayed = displayedInputs()
+
+//   return (
+//     <div>
+//       {displayed}
+//     </div>
+//   );
+// };
+
+// export default GeneratedThread;
+
+const GeneratedThread = (numThreads, thread, textArea = []) => {
   const [shown, setShown] = useState(["", "", "", ""])
   const [oldThread, setOldThread] = useState(thread)
+
+  //saves the last generated thread and changes whats shown if a new thread is generated
   if (oldThread !== thread) {
     setOldThread(thread)
     setShown(thread)
   }
 
-  const displayedInputs = () => {
-    const inputs = [];
-    for (var i = 0; i < numberOfThreads; i++) {
-      inputs.push(
-        <textarea
-          id="1"
-          className="Input"
-          maxLength="280"
-          value = {shown[i]}
-          onChange={(e) => setShown(shown.splice(i, 1, e.target.value))}
-        />
-      );
-    }
-    return inputs;
-  };
+  if (numThreads >= textArea.length) {
+    return (
+      <div>
+        {textArea}
+      </div>
+    )
+  }
+  
+  //this is a little messy, here is what is happening:
+  //the threads that are generated are in an array so i need to 
+  //get them out of the array an into the value of the text area
+  //so i used textarea.length as the index value since it starts at 0
+  //and increases by one every function call
+  const newTextArea = 
+  <textarea
+    className="Input"
+    maxLength="280"
+    value={shown[textArea.length]}
+    onChange={(e) => setShown(shown.splice(textArea.length, 1, e.target.value))}
+  />
 
-  const displayed = displayedInputs()
+  return GeneratedThread(numThreads, thread, [...textArea, newTextArea])
+}
 
-  return (
-    <div>
-      {displayed}
-    </div>
-  );
-};
-
-export default GeneratedThread;
+export default GeneratedThread
