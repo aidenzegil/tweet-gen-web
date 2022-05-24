38896a334d208ee663fcb1a5bd9b45b85481621f
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-24 12:43:22 -0400

    fixed component

--- src/components/GeneratedThread.js
+++ src/components/GeneratedThread.js
@@ -1,38 +1,43 @@
 import React from "react";
 import { useState } from "react";
 
-const GeneratedThread = (numThreads, thread, textArea = []) => {
-  const [shown, setShown] = useState(["", "", "", ""])
-  const [oldThread, setOldThread] = useState(thread)
+const GeneratedThread = (numThreads, thread) => {
+const [shown, setShown] = useState([" ", " ", " ", " "])
+const [oldThread, setOldThread] = useState(thread)  
 
-  //saves the last generated thread and changes whats shown if a new thread is generated
+  //saves the last generated thread and changes what's shown if a new thread is generated
   if (oldThread !== thread) {
     setOldThread(thread)
     setShown(thread)
   }
 
-  if (numThreads >= textArea.length) {
-    return (
-      <div>
-        {textArea}
-      </div>
-    )
-  }
-  
-  //this is a little messy, here is what is happening:
-  //the threads that are generated are in an array so i need to 
-  //get them out of the array an into the value of the text area
-  //so i used textarea.length as the index value since it starts at 0
-  //and increases by one every function call
-  const newTextArea = 
-  <textarea
-    className="Input"
-    maxLength="280"
-    value={shown[textArea.length]}
-    onChange={(e) => setShown(shown.splice(textArea.length, 1, e.target.value))}
-  />
+  var index = 0
 
-  return GeneratedThread(numThreads, thread, [...textArea, newTextArea])
+  function renderThreads(numThreads, textArea = []) {
+    if (numThreads <= textArea.length) {
+      return (
+        <div>
+          {textArea}
+        </div>
+      )
+    }
+    
+    //this is a little messy, here is what is happening:
+    //the threads that are generated are in an array so i need to 
+    //get them out of the array an into the value of the text area
+    //so i used textarea.length as the index value since it starts at 0
+    //and increases by one every function call
+    const newTextArea = 
+    <textarea
+      className="Input"
+      maxLength="280"
+      value={shown[index]}
+      onChange={(e) => setShown(shown.splice(index, 1, e.target.value))}
+    />
+    index++
+    return renderThreads(numThreads, [...textArea, newTextArea])
+  }
+  return renderThreads(numThreads)
 }
 
 export default GeneratedThread
