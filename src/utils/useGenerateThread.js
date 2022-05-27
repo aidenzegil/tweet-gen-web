ab4cdb12a735171cbb53855f65f1e19ad7467e9f
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-27 12:12:50 -0400

    updated fetch

--- src/utils/useGenerateThread.js
+++ src/utils/useGenerateThread.js
@@ -6,7 +6,7 @@ async function generateThread(tweet, numThreads, thread = []) {
   
   const prompt = "Continue "  + [tweet, ...thread].join("") + "\n"
 
-  const result = await fetch("http://127.0.0.1:8000/tweet/", {
+  const result = await fetch("http://tweeterman.herokuapp.com/tweet/", {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
